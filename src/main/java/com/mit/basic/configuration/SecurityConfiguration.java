97e2c7a5f6699a2af670d582cab367f41cc91fa1
tawakal <fairtawakal@gmail.com>
2021-03-07 17:03:44 +0700

    securityconfiguration

--- src/main/java/com/mit/basic/configuration/SecurityConfiguration.java
+++ src/main/java/com/mit/basic/configuration/SecurityConfiguration.java
@@ -1,9 +1,41 @@
 package com.mit.basic.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import com.mit.basic.service.UserService;
 
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
 
+	@Autowired
+	private UserService userService;
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+		auth.userDetailsService(userService);
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable().authorizeRequests().antMatchers("/api/user/register")
+		.permitAll().anyRequest().authenticated();
+	}
+	
+	public PasswordEncoder passwordEncoder() {
+		return NoOpPasswordEncoder.getInstance();
+	}
+	
+	@Override
+	protected AuthenticationManager authenticationManager() throws Exception {
+		return super.authenticationManager();
+	}
+	
+	
 }
--- src/main/java/com/mit/basic/controller/AuthController.java
+++ src/main/java/com/mit/basic/controller/AuthController.java
@@ -3,20 +3,24 @@ package com.mit.basic.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.mit.basic.model.UserModel;
 import com.mit.basic.service.AuthService;
 
 @RestController
+@Controller
+@RequestMapping(value = "/api/user")
 public class AuthController {
 
 	@Autowired
 	private AuthService authService;
 	
-	@PostMapping
+	@PostMapping("/register")
 	public ResponseEntity<?> registerUser(@RequestBody UserModel user){
 		try {
 			return ResponseEntity.ok(authService.registerUser(user));
